4d0083f8439b129b0ab1b06bede8d652d25fc71c
Artur <Artur.Hoppner@hotmail.com>
2020-05-01 13:51:11 +0200

    Initiating project

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+<h1>Pendlaren</h1>
+
+<button>Geolocation</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
