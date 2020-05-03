a292b6e5900ab907b26af2b5382436cecd54eb33
Artur <Artur.Hoppner@hotmail.com>
2020-05-03 14:47:32 +0200

    added displatch api for get station and push it into state.stations[]

--- src/store/index.js
+++ src/store/index.js
@@ -5,20 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-      geolocation: {},
+      geolocation: [],
       stations: [],
       bussDeparture: []
 
   },
   mutations: {
-    addGeolocation(state, item) {
-      console.log(item, "current, location mutation")
-      state.geolocation.push();
+    addGeolocation (state, item) {
+      console.log(item.longitude, "current, location mutation")
+      state.geolocation.push(item)
+      console.log(state.geolocation, "state")
+
     },
 
-    // addStations(state, item) {
-    //   state.stations.push(item)
-    // },
+    addStations(state, item) {
+      state.stations.push(item)
+      console.log(state.stations, "station in state")
+    },
     // addbussDeparture(state, item) {
     //   state.bussDeparture.push({item})
     // },
@@ -26,40 +29,41 @@ export default new Vuex.Store({
   },
 
   actions: {
-     getLocation({commit}) {
+     getLocation({commit, dispatch}) {
       if (navigator.geolocation) {
            navigator.geolocation.getCurrentPosition(  (position) =>{
              console.log("get")
              let currentLocation = position.coords;
-             console.log(currentLocation, "current, location action")
+             console.log(currentLocation, "current, location action");
+             commit("addGeolocation", currentLocation);
+             dispatch('getReseplanerare');
 
-             commit("addGeolocation", currentLocation)
-            //  store.dispatch(getReseplanerare(currentLocation));       
          })} else {
              console.log("Error, cant get Geolocation!");
          }
-     }
+     },
 
-    //  async getReseplanerare({commit}, currentLocation) {
-    //   const apiKeyReseplanerare = '52f3c92f-0f19-4ac4-92d1-72da58f3e3ec'
-    //   const baseUrlReseplanerare = "https://api.resrobot.se/v2/location.nearbystops?"
-    //   let latitude = currentLocation.latitude;
-    //   console.log("Getting Latitude;", latitude)
-    //   let longitude = currentLocation.longitude;
-    //   console.log("Getting Longitude;",longitude)
+     async getReseplanerare({commit}) {
+       console.log("stating getReseplanerare")
+      // const apiKeyReseplanerare = '52f3c92f-0f19-4ac4-92d1-72da58f3e3ec'
+      // const baseUrlReseplanerare = "https://api.resrobot.se/v2/location.nearbystops?"
+      // let latitude = currentLocation.latitude;
+      // console.log("Getting Latitude;", latitude)
+      // let longitude = currentLocation.longitude;
+      // console.log("Getting Longitude;",longitude)
     
-    //     url = `${baseUrlReseplanerare}key=${apiKeyReseplanerare}&originCoordLat=${latitude}&originCoordLong=${longitude}&format=json`;
-      
-    //   try{
-    //     let response = await fetch(url);
-    //     let data = await response.json();
-    //     commit('addStations', data)
+      //   url = `https://api.resrobot.se/v2/location.nearbystops?key=52f3c92f-0f19-4ac4-92d1-72da58f3e3ec&originCoordLat=59.30352639999999&originCoordLong=18.1403648&format=json`;
+      try{
+        let response = await fetch('https://api.resrobot.se/v2/location.nearbystops?key=52f3c92f-0f19-4ac4-92d1-72da58f3e3ec&originCoordLat=59.30352639999999&originCoordLong=18.1403648&format=json');
+        let data = await response.json();
+        commit('addStations', data)
+        console.log(data, "data in async")
 
-    //     } catch (err) {
-    //       console.log(error, "error getReseplanerare", "9");
-    //   }
-    // return
-    // }
+        } catch (err) {
+          console.log(error, "error getReseplanerare", "9");
+      }
+    return
+    }
   },
 
   modules: {
